4ac04414424f46358128c8ee11281b40513a5e56
VenkateshSR06 <venkatesh.sr781@gmail.com>
2025-12-02 22:29:20 +0530

    Update list-github-users.sh

--- list-github-users.sh
+++ list-github-users.sh
@@ -1,32 +1,31 @@
 #!/bin/bash
 API_URL="https://api.github.com"
 
-USER_NAME=$username
-TOKEN=$token
+USER_NAME="$username"
+TOKEN="$token"
 
-REPO_OWNER=$1
-REPO_NAME=$2
+REPO_OWNER="$1"
+REPO_NAME="$2"
 
-function github_api_get{
-local endpoint=$1
-local url="$(API_URL)/$(endpoint)"
+function github_api_get(){
+local endpoint="$1"
+local url="${API_URL}/${endpoint}"
 
 curl -s -u "$USER_NAME:$TOKEN" "$url"
 }
 
-function list_users{
-local endpoint="repos/$(REPO_OWNER)/$(REPO_NAME)/collaborators"
+function list_users(){
+local endpoint="repos/${REPO_OWNER}/${REPO_NAME}/collaborators"
 
-collaborators=$(github_api_get $endpoint | jq -r '.[] | select(.permissions.pull == true) | .login)
-
-if [ -z $(collaborators) ];
+collaborators="$(github_api_get $endpoint | jq -r '.[] | select(.permissions.pull == true) | .login')"
+if [ -z $collaborators ];
 then
-   echo " No users with read access for repo $(REPO_OWNER)/$(REPO_NAME)"
+   echo " No users with read access for repo ${REPO_OWNER}/${REPO_NAME}"
 else
-   echo " Users with read access for repo $(REPO_OWNER)/$(REPO_NAME)"
-   echo "$(collabarators)"
+   echo " Users with read access for repo ${REPO_OWNER}/${REPO_NAME}"
+   echo "$collaborators"
 fi
 }
 
-echo "Listing users of repo $(REPO_OWNER)/$(REPO_NAME)
+echo "Listing users of repo  ${REPO_OWNER}/${REPO_NAME}"
 list_users
